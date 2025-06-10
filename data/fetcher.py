52daff49ced2c7d7cc8184670d1fe7e1b699248d
Aida Shakharova <109811676+aidoksmile@users.noreply.github.com>
2025-06-10 14:54:26 +0500

    Update fetcher.py

--- data/fetcher.py
+++ data/fetcher.py
@@ -2,7 +2,7 @@ import yfinance as yf
 
 def fetch_data(ticker, interval='1d', period='1y'):
     try:
-        data = yf.download('GC=F', interval=interval, period=period)
+        data = yf.download('GC=F', interval='1d', period='1y')
         return data
     except Exception as e:
         print(f"Error fetching data: {e}")
